71fb2d053625d235f5d1cd507c45711d382ec5ed
danial.ygt <danialyaghooti@gmail.com>
2025-03-30 13:42:30 +0330

    .

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module Bookings
 
 go 1.24.1
+
+require (
+	github.com/alexedwards/scs v1.4.1 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+)
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,8 +1,10 @@
 package config
 
+import "github.com/alexedwards/scs"
+
 type AppConfig struct {
 	InProduction  bool
-	Session       *scs.SessionManager
+	Session       *scs.Session
 	UseCache      bool
 	TemplateCache map[string]*template.Template
 }
